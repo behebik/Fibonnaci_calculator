01197cd4b3ec71b762bff38181c4aa2c675acc26
behebik <Rahul@rahuls-mbp.gateway.2wire.net>
2014-06-16 17:26:03 -0400

    Eliminated irrelevant code

--- fibonnaci.py
+++ fibonnaci.py
@@ -14,15 +14,7 @@ def findme(limit):
         last_sum=new_sum
         current_sum+=new_sum
         print new_sum
-        #print "newsum" + str(new_sum)
-        #print "current_sum" + str(current_sum)
         if count == d_count-1:
-            #print current_sum
-            #confirm=str.upper(raw_input("Try again? Y/N"))
-            #if confirm == "Y":
-            #    findme(int(raw_input("How many digits?")))
-            #else:
-            #    break
             break
             
 findme(d_count)
